c3dfcdf1f69079a284b39e014f7ef4317083f7c1
joshday <emailjoshday@gmail.com>
2021-03-24 12:30:25 -0400

    working on select

--- src/AtomicTables.jl
+++ src/AtomicTables.jl
@@ -1,10 +1,14 @@
 module AtomicTables
 
-const ColumnTable = NamedTuple{names,T} where T<:Tuple{Vararg{AbstractArray{S,D} where S,N}} where names where D where N
+using NamedTupleTools
 
-export CTable
+import NamedTupleTools: select
+
+export CTable, cols, show_schema, Not, Before, After, Between
 
 #-----------------------------------------------------------------------# utils
+const ColumnTable = NamedTuple{names,T} where T<:Tuple{Vararg{AbstractArray{S,D} where S,N}} where names where D where N
+
 # Generate a name different from the provided names
 function uniquename(kys::NTuple{N, Symbol}) where {N}
     name = :x1
@@ -41,10 +45,13 @@ Base.summary(t::CTable) = "CTable ($(nrows(t)) × $(ncols(t)))"
 Base.length(t::CTable) = length(cols(t)[1])
 Base.pairs(t::CTable) = pairs(cols(t))
 Base.keys(t::CTable) = keys(cols(t))
+Base.size(t::CTable) = (nrows(t), ncols(t))
+Base.size(t::CTable, i::Integer) = size(t)[i]
 
 Base.getindex(t::CTable, i::Integer) = map(x -> x[i], cols(t))
 Base.getindex(t::CTable, i::AbstractVector{<:Integer}) = CTable(map(x -> x[i], cols(t)))
 Base.getindex(t::CTable, x::Symbol) = cols(t)[x]
+Base.getindex(t::CTable, x::AbstractVector{Symbol}) = CTable(select(cols(t), x))
 function Base.setindex!(t::CTable, val, i::Integer)
     for (k,v) in pairs(t)
         v[i] = val[k]
@@ -61,12 +68,15 @@ function Base.setproperty!(t::CTable, val, x::Symbol)
     val
 end
 
-#-----------------------------------------------------------------------------# iterate 
 Base.iterate(t::CTable, i=1) = i > length(t) ? nothing : (t[i], i + 1)
+Base.eltype(t::CTable) = NamedTuple{keys(t), Tuple{map(eltype, cols(t))...}}
+
+Base.merge(a::CTable, b::CTable) = CTable(merge(cols(a), cols(b)))
 
 #-----------------------------------------------------------------------# includes
 # include("tables_extensions.jl")
 # include("ctable.jl")
 include("show.jl")
+include("select.jl")
 
 end # module
